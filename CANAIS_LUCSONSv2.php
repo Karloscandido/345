ca3c61a5c9fd9771fcf6cf96e0e92953a1dd8d3a
Karloscandido <120978870+Karloscandido@users.noreply.github.com>
2023-08-15 16:45:24 -0400

    Add files via upload

---
+++ CANAIS_LUCSONSv2.php
@@ -0,0 +1,335 @@
+#EXTM3U
+#EXTINF:-1,TNT Series 4k
+https://br4jul.walk-tv.com/live/5904_.m3u8?token 
+#EXTINF:-1,Telecine Touch 4k
+https://br4jul.walk-tv.com/live/5880_.m3u8?token 
+#EXTINF:-1,Telecine Premium 4k
+https://br4jul.walk-tv.com/live/5879_.m3u8?token 
+#EXTINF:-1,Telecine Pipoca 4k
+https://br4jul.walk-tv.com/live/5877_.m3u8?token 
+#EXTINF:-1,Telecine Fun 4k
+https://br4jul.walk-tv.com/live/5901_.m3u8?token 
+#EXTINF:-1,Telecine Cult 4k
+https://br4jul.walk-tv.com/live/5899_.m3u8?token 
+#EXTINF:-1,Telecine Action 4k
+https://br4jul.walk-tv.com/live/5875_.m3u8?token 
+#EXTINF:-1,Space 4k
+https://br4jul.walk-tv.com/live/5771_.m3u8?token 
+#EXTINF:-1,Life Time 4k
+https://br4jul.walk-tv.com/live/5816_.m3u8?token 
+#EXTINF:-1,Megapix 4k
+https://br4jul.walk-tv.com/live/5897_.m3u8?token 
+#EXTINF:-1,ESPN 4
+https://br4jul.walk-tv.com/live/6586_.m3u8?token
+#EXTINF:-1,HBO Xtreme 4k
+https://br4jul.walk-tv.com/live/88378_.m3u8?token 
+#EXTINF:-1,HBO Pop 4k
+https://br4jul.walk-tv.com/live/88380_.m3u8?token 
+#EXTINF:-1,HBO Mundi 4k
+https://br4jul.walk-tv.com/live/88377_.m3u8?token 
+#EXTINF:-1,HBO Signature 4k
+https://br4jul.walk-tv.com/live/5861_.m3u8?token 
+#EXTINF:-1,HBO Family 4k
+https://br4jul.walk-tv.com/live/5859_.m3u8?token 
+#EXTINF:-1,HBO Plus 4k
+https://br4jul.walk-tv.com/live/5893_.m3u8?token 
+#EXTINF:-1,HBO 2 4k
+https://br4jul.walk-tv.com/live/5857_.m3u8?token 
+#EXTINF:-1,HBO 4k
+https://br4jul.walk-tv.com/live/5891_.m3u8?token 
+#EXTINF:-1,VerAinda
+https://br4jul.walk-tv.com/live/5773_.m3u8?token
+#EXTINF:-1,Tru TV 4k
+https://br4jul.walk-tv.com/live/88555_.m3u8?token 
+#EXTINF:-1,Warner Channel 4k
+https://br4jul.walk-tv.com/live/5882_.m3u8?token 
+#EXTINF:-1,Universal Channel 4k
+https://br4jul.walk-tv.com/live/5847_.m3u8?token 
+#EXTINF:-1,Film 4k
+https://br4jul.walk-tv.com/live/5727_.m3u8?token 
+#EXTINF:-1,TNT 4k
+https://br4jul.walk-tv.com/live/5781_.m3u8?token 
+#EXTINF:-1,TLC 4k
+https://br4jul.walk-tv.com/live/5902_.m3u8?token
+#EXTINF:-1,Globo
+https://br4jul.walk-tv.com/live/5755_.m3u8?token
+#EXTINF:-1,TCM 4k
+https://br4jul.walk-tv.com/live/5837_.m3u8?token
+#EXTINF:-1,ESPN HD
+https://br4jul.walk-tv.com/live/5912_.m3u8?token
+#EXTINF:-1,ESPN 2 HD
+https://br4jul.walk-tv.com/live/5910_.m3u8?token
+#EXTINF:-1,SyFy 4k
+https://br4jul.walk-tv.com/live/5835_.m3u8?token 
+#EXTINF:-1,SBT 4k
+https://br4jul.walk-tv.com/live/5736_.m3u8?token
+#EXTINF:-1,Fox Sport 2
+https://br4jul.walk-tv.com/live/5924_.m3u8?token
+#EXTINF:-1,Studio Universal 4k
+https://br4jul.walk-tv.com/live/5834_.m3u8?token 
+#EXTINF:-1,Paramount 4k
+https://br4jul.walk-tv.com/live/5827_.m3u8?token 
+#EXTINF:-1,Premiere
+https://br4jul.walk-tv.com/live/5914_.m3u8?token
+#EXTINF:-1,Premiere 2
+https://br4jul.walk-tv.com/live/5916_.m3u8?token
+#EXTINF:-1,Premiere
+https://br4jul.walk-tv.com/live/5916_.m3u8?token
+#EXTINF:-1,FX 4k
+https://br4jul.walk-tv.com/live/5855_.m3u8?token 
+#EXTINF:-1,Star Channel 1 4k
+https://br4jul.walk-tv.com/live/5889_.m3u8?token 
+#EXTINF:-1,Cine Canal 4k
+https://br4jul.walk-tv.com/live/5869_.m3u8?token 
+#EXTINF:-1,STAR CHANNEL 4K
+https://br4jul.walk-tv.com/live/5888_.m3u8?token 
+#EXTINF:-1,SporTv HD
+https://br4jul.walk-tv.com/live/5922_.m3u8?token
+#EXTINF:-1,SporTv 4k
+https://br4jul.walk-tv.com/live/5932_.m3u8?token
+#EXTINF:-1,SporTv 3 4k
+https://br4jul.walk-tv.com/live/5930_.m3u8?token
+#EXTINF:-1,Premiere
+https://br4jul.walk-tv.com/live/5920_.m3u8?token
+#EXTINF:-1,Globo 4k
+https://br4jul.walk-tv.com/live/88345_.m3u8?token
+#EXTINF:-1,PlayBoy HBO Extreme
+https://br4jul.walk-tv.com/live/88378_.m3u8?token
+#EXTINF:-1,SemSinal
+https://br4jul.walk-tv.com/live/88524_.m3u8?token
+#EXTINF:-1,SemSinal
+https://br4jul.walk-tv.com/live/88526_.m3u8?token
+#EXTINF:-1,Globo Minas Gerais
+https://br4jul.walk-tv.com/live/88525_.m3u8?token
+#EXTINF:-1,TruTb
+https://br4jul.walk-tv.com/live/88555_.m3u8?token
+#EXTINF:-1,Fox News Internacional
+https://br4jul.walk-tv.com/live/88571_.m3u8?token
+#EXTINF:-1,Globo Minas
+https://br4jul.walk-tv.com/live/88609_.m3u8?token
+#EXTINF:-1,Globo Santa Catarina
+https://br4jul.walk-tv.com/live/88615_.m3u8?token
+#EXTINF:-1,Globo
+https://br4jul.walk-tv.com/live/5758_.m3u8?token
+#EXTINF:-1,WorldTv 4k
+https://br4jul.walk-tv.com/live/5886_.m3u8?token
+#EXTINF:-1,Comedy Central 4k
+https://br4jul.walk-tv.com/live/5867_.m3u8?token 
+#EXTINF:-1,Canal Sony 4k
+https://br4jul.walk-tv.com/live/5799_.m3u8?token 
+#EXTINF:-1,Canal Brasil 4k
+https://br4jul.walk-tv.com/live/5796_.m3u8?token 
+#EXTINF:-1,Combate 4k
+https://br4jul.walk-tv.com/live/5865_.m3u8?token
+#EXTINF:-1,A&E 4k
+https://br4jul.walk-tv.com/live/5790_.m3u8?token 
+#EXTINF:-1,AXN 4k
+http://br5julflu.walk-tv.com/axnbr/video.m3u8?token
+#EXTINF:-1,Premiere
+https://br4jul.walk-tv.com/live/5918_.m3u8?token
+#EXTINF:-1,Cartoonito 4k
+https://br4jul.walk-tv.com/live/5767_.m3u8?token 
+#EXTINF:-1,E 
+https://br4jul.walk-tv.com/live/5729_.m3u8?token
+#EXTINF:-1,TV RA TIM BUM 4k
+https://br4jul.walk-tv.com/live/5844_.m3u8?token 
+#EXTINF:-1,Tooncast 
+https://br4jul.walk-tv.com/live/5838_.m3u8?token 
+#EXTINF:-1,Nickelodeon 4k
+https://br4jul.walk-tv.com/live/5825_.m3u8?token 
+#EXTINF:-1,Nick Jr. 4k
+https://br4jul.walk-tv.com/live/5779_.m3u8?token 
+#EXTINF:-1,Discovery Kids 4k
+https://br4jul.walk-tv.com/live/5806_.m3u8?token 
+#EXTINF:-1,Nat Geo Kids 4k
+https://br4jul.walk-tv.com/live/5872_.m3u8?token 
+#EXTINF:-1,Disney XD 4k
+https://br4jul.walk-tv.com/live/5776_.m3u8?token 
+#EXTINF:-1,Disney Junior 4k
+https://br4jul.walk-tv.com/live/5775_.m3u8?token 
+#EXTINF:-1,Disney Channel 4k
+https://br4jul.walk-tv.com/live/5787_.m3u8?token 
+#EXTINF:-1,Cartoon Network 4k
+https://br4jul.walk-tv.com/live/5801_.m3u8?token 
+#EXTINF:-1,Baby Tv
+https://br4jul.walk-tv.com/live/5766_.m3u8?token 
+#EXTINF:-1,PLAYKIDS 
+encurtador.com.br/lovIP
+#EXTINF:-1,BBB 2023 Casa De Vidro
+https://br4jul.walk-tv.com/live/87771_.m3u8?token 
+#EXTINF:-1,MultiShow 4k
+https://br4jul.walk-tv.com/live/5822_.m3u8?token 
+#EXTINF:-1,MTV 4k
+https://br4jul.walk-tv.com/live/5821_.m3u8?token 
+#EXTINF:-1,Premiere 5
+https://br4jul.walk-tv.com/live/5915_.m3u8?token
+#EXTINF:-1,Discovery Channel HD
+https://br4jul.walk-tv.com/live/5785_.m3u8?token 
+#EXTINF:-1,†Animal Planet 4k
+https://br4jul.walk-tv.com/live/5782_.m3u8?token 
+#EXTINF:-1,A&E 4k
+https://br4jul.walk-tv.com/live/5790_.m3u8?token 
+#EXTINF:-1,Globinho 4K 
+https://br4jul.walk-tv.com/live/5812_.m3u8?token
+#EXTINF:-1,£Fox News 4k
+https://br4jul.walk-tv.com/live/88571_.m3u8?token 
+#EXTINF:-1,CNN Internacional 4k
+https://br4jul.walk-tv.com/live/88573_.m3u8?token 
+#EXTINF:-1,CNN Espanhol 4k
+https://br4jul.walk-tv.com/live/88566_.m3u8?token 
+#EXTINF:-1,Band News 4k
+https://br4jul.walk-tv.com/live/5793_.m3u8?token 
+#EXTINF:-1,ARTE 1 4k
+https://br4jul.walk-tv.com/live/5791_.m3u8?token
+#EXTINF:-1,BIS 4k
+https://br4jul.walk-tv.com/live/5794_.m3u8?token 
+#EXTINF:-1,Disc. Home & Health 4k
+https://br4jul.walk-tv.com/live/5784_.m3u8?token 
+#EXTINF:-1,£E! 4k
+https://br4jul.walk-tv.com/live/5729_.m3u8?token 
+#EXTINF:-1,+Globosat 4k
+https://br4jul.walk-tv.com/live/5817_.m3u8?token 
+#EXTINF:-1,VH1 4k
+https://br4jul.walk-tv.com/live/5848_.m3u8?token 
+#EXTINF:-1,VIVA 4k
+https://br4jul.walk-tv.com/live/5850_.m3u8?token 
+#EXTINF:-1,†MusicBox Brazil 4k
+https://br4jul.walk-tv.com/live/5823_.m3u8?token 
+#EXTINF:-1,TBS 4k
+https://br4jul.walk-tv.com/live/5836_.m3u8?token 
+#EXTINF:-1,Gnt 4k
+https://br4jul.walk-tv.com/live/5813_.m3u8?token 
+#EXTINF:-1,Globo News 4k
+https://br4jul.walk-tv.com/live/5811_.m3u8?token
+#EXTINF:-1,Woohoo
+https://br4jul.walk-tv.com/live/5851_.m3u8?token
+#EXTINF:-1,PlayBoy
+https://br4jul.walk-tv.com/live/5720_.m3u8?token
+#EXTINF:-1,AXW
+https://br4jul.walk-tv.com/live/5792_.m3u8?token
+#EXTINF:-1,OFF 4k
+https://br4jul.walk-tv.com/live/5826_.m3u8?token 
+#EXTINF:-1,H2 History
+https://br4jul.walk-tv.com/live/5814_.m3u8?token
+#EXTINF:-1,Box Brasil 4k
+https://br4jul.walk-tv.com/live/5829_.m3u8?token 
+#EXTINF:-1,POLISHOP TV 
+https://br4jul.walk-tv.com/live/60003_.m3u8?token 
+#EXTINF:-1,£NHK 4k
+;https://br4jul.walk-tv.com/live/5724_.m3u8?token 
+#EXTINF:-1,National Geographic 4k
+https://br4jul4k.walk-tv.com/live/5777_.m3u8?token 
+#EXTINF:-1,Globo
+https://br4jul.walk-tv.com/live/5752_.m3u8?token
+#EXTINF:-1,Globo 4k
+https://br4jul.walk-tv.com/live/5756_.m3u8?token
+#EXTINF:-1,SporTv 2
+https://br4jul.walk-tv.com/live/5931_.m3u8?token
+#EXTINF:-1,Globo
+https://br4jul.walk-tv.com/live/6431_.m3u8?token
+#EXTINF:-1,Turbo Discovery
+https://br4jul.walk-tv.com/live/5786_.m3u8?token
+#EXTINF:-1,Food
+https://br4jul.walk-tv.com/live/5759_.m3u8?token
+#EXTINF:-1,Curta
+https://br4jul.walk-tv.com/live/5804_.m3u8?token
+#EXTINF:-1,CineMax
+https://br4jul.walk-tv.com/live/5783_.m3u8?token
+#EXTINF:-1,Premiere 6
+https://br4jul.walk-tv.com/live/5921_.m3u8?token
+#EXTINF:-1,Band Sports
+https://br4jul.walk-tv.com/live/5928_.m3u8?token
+#EXTINF:-1,Camera Dos Bandidos
+https://br4jul.walk-tv.com/live/5840_.m3u8?token
+#EXTINF:-1,Nat Geo Wild 4K
+https://br4jul4k.walk-tv.com/live/5778_.m3u8?token 
+Nat-Geo-Wild-4k
+https://br4jul.walk-tv.com/live/5778_.m3u8?token 
+#EXTINF:-1,ID 4k
+https://br4jul.walk-tv.com/live/5895_.m3u8?token 
+#EXTINF:-1,HGTV 4k
+https://br4jul4k.walk-tv.com/live/5805_.m3u8?token 
+#EXTINF:-1,History-Channel-4k
+https://br4jul.walk-tv.com/live/5815_.m3u8?token 
+#EXTINF:-1,£History Channel 4K
+https://br4jul4k.walk-tv.com/live/5815_.m3u8?token 
+#EXTINF:-1,Cine Canal 4K
+https://br4jul.walk-tv.com/live/5869_.m3u8?token 
+#EXTINF:-1,Discovery Theater 4k
+https://br4jul.walk-tv.com/live/5853_.m3u8?token 
+#EXTINF:-1,Discovery Science 4k
+https://br4jul.walk-tv.com/live/5807_.m3u8?token 
+#EXTINF:-1,Discovery Science 4k
+https://br4jul.walk-tv.com/live/5810_.m3u8?token
+#EXTINF:-1,HGTV 4K
+https://br4jul.walk-tv.com/live/5805_.m3u8?token 
+#EXTINF:-1,Discovery Channel 4k
+https://br4jul.walk-tv.com/live/5785_.m3u8?token 
+#EXTINF:-1,JP News 4m
+https://br4jul.walk-tv.com/live/149504_.m3u8?token 
+#EXTINF:-1,Loading TV 4k
+https://br4jul.walk-tv.com/live/49_.m3u8?token
+#EXTINF:-1,ESPN 3
+https://br4jul.walk-tv.com/live/5907_.m3u8?token
+#EXTINF:-1,FishTV 4k
+https://br4jul.walk-tv.com/live/5808_.m3u8?token
+#EXTINF:-1,Tv senado 4k
+https://br4jul.walk-tv.com/live/5845_.m3u8?token 
+#EXTINF:-1,TV justi√a 4k
+https://br4jul.walk-tv.com/live/5842_.m3u8?token 
+#EXTINF:-1,TV brasil 4k
+https://br4jul.walk-tv.com/live/5772_.m3u8?token 
+#EXTINF:-1,TV aparecida 4k
+https://br4jul.walk-tv.com/live/5773_.m3u8?token 
+#EXTINF:-1,Sbt 4k
+https://br4jul.walk-tv.com/live/8888_.m3u8?token 
+#EXTINF:-1,Red super 4k
+https://br4jul.walk-tv.com/live/88616_.m3u8?token 
+#EXTINF:-1,Red genisis 4k
+https://br4jul.walk-tv.com/live/88617_.m3u8?token 
+#EXTINF:-1,Red tv 4k
+https://br4jul.walk-tv.com/live/5738_.m3u8?token 
+#EXTINF:-1,Red tv 4k
+https://br4jul.walk-tv.com/live/5738_.m3u8?token 
+#EXTINF:-1,Rede Record 4k
+https://br4jul.walk-tv.com/live/5740_.m3u8?token 
+#EXTINF:-1,Record News 4k
+https://br4jul.walk-tv.com/live/5789_.m3u8?token 
+#EXTINF:-1,Band 4k
+https://br4jul.walk-tv.com/live/5763_.m3u8?token 
+#EXTINF:-1,Globo 4k
+https://br4jul.walk-tv.com/live/5754_.m3u8?token
+#EXTINF:-1,Prive
+https://br4jul.walk-tv.com/live/87774_.m3u8?token 
+#EXTINF:-1,Sexy Hot
+https://br4jul.walk-tv.com/live/5722_.m3u8?token
+#EXTINF:-1,FUNK 01
+https://servidor.geracaoradios.com/listen/funk/stream
+#EXTINF:-1,TvDiario 4k
+https://br4jul.walk-tv.com/live/5795_.m3u8?token
+#EXTINF:-1,FUNK 02
+https://superaudio.radio.br/radio/8270/stream
+#EXTINF:-1,FUNK 03
+http://stream.zeno.fm/kpm2ghgfnxhvv
+#EXTINF:-1,SERTANEJO 01
+https://stream.zeno.fm/8sm17s4u3gruv
+#EXTINF:-1,SERTANEJO 02
+https://live.hunter.fm/sertanejo_normal
+#EXTINF:-1,SERTANEJO 03
+https://s17.maxcast.com.br:8669/live
+#EXTINF:-1,FLASHBACK 01
+http://s1.nexuscast.com:8135/;
+#EXTINF:-1,FLASHBACK 02
+https://stream.zeno.fm/flnk47wp7bnvv
+#EXTINF:-1,FLASHBACK 03
+http://stream.laut.fm/flashbackgold
+#EXTINF:-1,HIP HOP 01
+https://servidor.geracaoradios.com/listen/hiphoprap/stream
+#EXTINF:-1,HIP HOP
+https://stm7.voxhd.com.br:9366/;
+#EXTINF:-1,HIP HOP
+https://central.ez1fm.com/rap/stream
+#EXTINF:-1,SemSinal
+https://br4jul.walk-tv.com/live/5750_.m3u8?token
+#EXTINF:-1,SemSinal2
+https://br4jul.walk-tv.com/live/5798_.m3u8?token
